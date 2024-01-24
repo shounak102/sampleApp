0f805c1c950aa3f7401afddc3895b3efbdc77f6a
shounak <ray.shounak@gmail.com>
2024-01-24 16:21:22 +0000

    updated scripts and modules

--- scripts/nodejs.sh
+++ scripts/nodejs.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-
+source ~/.bashrc
 # Check if NVM is installed
 if [ -s "$NVM_DIR/nvm.sh" ]; then
   # Load NVM
   . "$NVM_DIR/nvm.sh"
 
   # Install node
-  nvm install node
+  nvm install 21.6.0
+  nvm use 21.6.0
   # Your script commands here
   node --version
   npm --version
